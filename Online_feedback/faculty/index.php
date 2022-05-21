a74d56c3eeceb18955a883b3c2bff0f821c8a6f4
Nithu <ponsivanesannithu@gmail.com>
2022-05-22 00:38:39 +0530

    rr

--- Online_feedback/faculty/index.php
+++ Online_feedback/faculty/index.php
@@ -171,7 +171,7 @@ $users=mysqli_fetch_assoc($sql);
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ 
     <script>window.jQuery || document.write('<script src="../js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
